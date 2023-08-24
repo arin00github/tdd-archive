b52452e7a19183912125070f4716ea15e04b156d
otter0104 <otter0104@innodep.com>
2023-08-24 16:45:44 +0900

    💡Test: CreateUser_Success.test.tsx 의 코드 업데이트

--- src/components/tddBasic/CreateUser_Success.test.tsx
+++ src/components/tddBasic/CreateUser_Success.test.tsx
@@ -10,42 +10,19 @@ afterAll(() => {
   tddServer.close();
 });
 
-describe("Create User Component", () => {
-  it("Input Component Rendering & Change Value", async () => {
+describe("TEST: 유저생성 성공 테스트케이스", () => {
+  it("CreateUser-009", async () => {
     const utils = render(<CreateUser options={initialOptions} />);
     const inputName = utils.getByTestId("creatuser-input-name");
-
-    expect(inputName).toBeInTheDocument();
-
-    fireEvent.change(inputName, { target: { value: "LeeYunseo" } });
-
-    expect(inputName.getAttribute("value")).toBe("LeeYunseo");
-    expect(inputName.getAttribute("value")).toMatch(/^[A-Za-z]{1,20}$/);
-  });
-
-  it("Select Component Rendering & Change Value", async () => {
-    const utils = render(<CreateUser options={initialOptions} />);
     const groupSelect = utils.getByTestId(
       "create-select-group"
     ) as HTMLSelectElement;
-    expect(groupSelect).toBeInTheDocument();
+
+    fireEvent.change(inputName, { target: { value: "LeeYunseo" } });
     fireEvent.change(groupSelect, { target: { value: "TMS-group" } });
+
+    expect(inputName.getAttribute("value")).toBe("LeeYunseo");
     expect(groupSelect.value).toBe("TMS-group");
-    expect(groupSelect.childNodes.length).toEqual(initialOptions.length + 1);
-  });
-
-  it("Error Message about Input", async () => {
-    const utils = render(<CreateUser options={initialOptions} />);
-    const submitBtn = utils.getByTestId("submit-btn");
-    fireEvent.click(submitBtn);
-    expect(screen.getByText("모두 다 작성해 주세요")).toBeInTheDocument();
-  });
-
-  // options의 초기값 체크
-  it("Check Initial Options", async () => {
-    expect(initialOptions).toHaveLength(3);
-    const newOptionArray = initialOptions.map((opt) => opt.label);
-    expect(newOptionArray).toContain("TMS개발");
-    expect(newOptionArray).not.toContain("DMS개발");
+    //expect(inputName.getAttribute("value")).toMatch(/^[A-Za-z]{1,20}$/);
   });
 });
